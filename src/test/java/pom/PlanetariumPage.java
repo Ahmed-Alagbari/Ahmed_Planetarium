b4f056736be2fdd6acf6fcbb59c7447485027aff
Ahmed-Alagbari <fi3809@wayne.edu>
2024-12-19 13:51:56 -0500

    test

--- src/test/java/pom/PlanetariumPage.java
+++ src/test/java/pom/PlanetariumPage.java
@@ -3,6 +3,7 @@ package pom;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PlanetariumPage{
     private WebDriver driver;
@@ -19,5 +20,11 @@ public class PlanetariumPage{
     @FindBy(xpath="//input[@type='submit']")
     private WebElement loginButton;
 
+
+
+    public PlanetariumPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
 
 }
